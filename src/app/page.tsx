5d87a81489af0336d3430ed4bd30f84fd7b0ba80
Mahmoud BanyAmer <Mahmoudbanyamer99@gmail.com>
2023-09-08 20:36:46 +0300

    test git from vs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>main</h1>
+			<h1>main Test</h1>
 		</main>
 	);
 }
